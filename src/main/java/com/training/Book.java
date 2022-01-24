dc5d5e557a7a4a1fccace660dff83329eddc628f
yannam04 <yannam04@gmail.com>
2022-01-24 21:05:36 +0800

    Initial commit

---
+++ src/main/java/com/training/Book.java
@@ -0,0 +1,44 @@
+package com.training;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "book", schema = "practice_schema")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Book() {
+    }
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
